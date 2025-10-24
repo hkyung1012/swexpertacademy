d815adef2612330a8af78ca624e84d1bd5c33bda
leehyunkyung <ten101ww@gmail.com>
2025-10-24 22:28:52 +0900

    251024

--- Basic/알고리즘/22_2579_계단오르기.py
+++ Basic/알고리즘/22_2579_계단오르기.py
@@ -13,13 +13,6 @@ sys.stdin = open("2579_input.txt", "r")
 # 입력: 입력의 첫째 줄에 계단의 개수
 # 출력: 첫째 줄에 계단 오르기 게임에서 얻을 수 있는 총 점수의 최댓값
 
-T = int(input())
-print(T)
-
-for i in range(T):
-    T1 = int(sys.stdin.readline().strip())
-    print(T1)
-
 # 최대 점수를 계산하는 동적 계획법 함수 (단일 테스트 케이스 처리)
 def solve_stair_climbing_case():
     try:
@@ -38,6 +31,7 @@ def solve_stair_climbing_case():
     scores = [0] + [int(sys.stdin.readline().strip()) for _ in range(N)]
             # for _ : **"반복 횟수는 필요하지만, 반복되는 동안의 각 순서(인덱스 또는 값)는 사용하지 않겠다"*
 
+    # 예외처리 (N=1, N=2)
     if N == 1:
         return scores[1]
 
@@ -48,9 +42,30 @@ def solve_stair_climbing_case():
     # dp[i][1]: i번째 계단을 밟았고, 직전에 1칸을 점프해 온 경우의 최대 점수
     # dp[i][2]: i번째 계단을 밟았고, 직전에 2칸을 점프해 온 경우의 최대 점수
     dp = [ [0] * 3 for _ in range(N + 1)]
-         # [0, 0, 0] => 1 row
-    
+         # [0, 0, 0] => 1 row  * (N+1)
 
     # 1번째 계단
     dp[1][1] = scores[1]
     dp[1][2] = scores[1]
+
+    # 2번째 계단
+    dp[2][1] = scores[2] + dp[1][1] # 1칸 점프: (1,2)
+    dp[2][2] = scores[2]            # 2칸 점프: (2)
+
+    # 점화식을 이용한 반복 계산 ( i =3 ~ N)
+    for i in range(3, N + 1):
+        # Case 1: i 번째 계단을 1칸 점프로 밟음 (i-1 -> i)
+        # -> 연속 3칸 금지 규칙 때문에, i-1 은 반드시 i-3 에서 2칸 점프로 왔어야 함
+        # (i-1 은 [2] 상태만 가능)
+        dp[i][1] = scores[i] + dp[i-1][2]
+
+        # Case 2: i번째 계단을 2칸 점프로 밟음 (i-2 -> i)
+        # -> i-1을 건너뛰었으므로, i-2는 어떤 상태든 상관 없음.
+        dp[i][2] = scores[i] + max(dp[i-2][1], dp[i-2][2])
+
+    print(dp)
+    return max(dp[N][1], dp[N][2])
+
+
+
+solve_stair_climbing_case()
