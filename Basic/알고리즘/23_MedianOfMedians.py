240f0bebb4f1be3e136742ebc5f8201e0e053793
leehyunkyung <ten101ww@gmail.com>
2025-11-04 00:03:30 +0900

    ex

--- Basic/알고리즘/23_MedianOfMedians.py
+++ Basic/알고리즘/23_MedianOfMedians.py
@@ -1,3 +1,8 @@
 """
 https://2jinishappy.tistory.com/127
+Quick Select를 진행하면서 선정한 pivot이 너무 작거나 크다면,
+partitioning이 제대로 이루어지지 않아 Worst-Case에 O(n²)이 소모됨을 확인했다
+그렇기 때문에, Quick Select의 성능은 'good pivot을 얼마나 빨리 찾아내느냐'에 달려있다
+이번에 다룰 Median of Medians 알고리즘은 good pivot을 worst-case O(n)에 찾아내는 알고리즘이다.
 """
+
